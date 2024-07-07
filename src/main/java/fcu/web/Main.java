58bdc999afcdea41605d2e02de3fbadc419a538d
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-07 22:47:07 +0800

    v1.545412 commit n push

--- src/main/java/fcu/web/Main.java
+++ src/main/java/fcu/web/Main.java
@@ -50,105 +50,132 @@ class FromEarliestToTheLatest
 public class Main {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.vscinemas.com.tw/vsweb/film/index.aspx");
-        String title = driver.getTitle();
-        System.out.println(title); //print out webPageTitle
-        List<FromEarliestToTheLatest> dates = new ArrayList<>();
-        //Create arrayList to input date into ArrayList in order to compare from the earliest to the latest
-        try (
-                FileWriter writer = new FileWriter("Movie.csv");
-                CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("電影名稱", "英文名稱", "上映日期", "下映日期"))) {
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.vscinemas.com.tw/vsweb/film/index.aspx");
+    String title = driver.getTitle();
+    System.out.println(title); //print out webPageTitle
+    List<FromEarliestToTheLatest> dates = new ArrayList<>();
+    //Create arrayList to input date into ArrayList in order to compare from the earliest to the latest
+    try (
+    FileWriter writer = new FileWriter("Movie.csv");
+    CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("電影名稱", "英文名稱", "上映日期", "下映日期"))) {
 
-            List<WebElement> elements = driver.findElements(By.cssSelector(".infoArea"));
-        int count=0;
+    List<WebElement> elements = driver.findElements(By.cssSelector(".infoArea"));
+    int count=0;
 
-            for (WebElement element : elements) {
-                WebElement nameElement = element.findElement(By.cssSelector("a"));
-                WebElement engNameElement = element.findElement(By.cssSelector("h3"));
-                WebElement timeNameElement = element.findElement(By.cssSelector("time"));
+for (WebElement element : elements) {
+    WebElement nameElement = element.findElement(By.cssSelector("a"));
+    WebElement engNameElement = element.findElement(By.cssSelector("h3"));
+    WebElement timeNameElement = element.findElement(By.cssSelector("time"));
 
-                DateTime now = new DateTime();
-                DateTimeFormatter format = DateTimeFormat.forPattern("yyyy-MM-dd");
-                DateTime day40 = now.plusDays(40);
-                String MovieTime = timeNameElement.getText();
-                DateTime MovieTimedates = format.parseDateTime(MovieTime);
+    DateTime now = new DateTime();
+    DateTimeFormatter format = DateTimeFormat.forPattern("yyyy-MM-dd");
+    DateTime day40 = now.plusDays(40);
+    String MovieTime = timeNameElement.getText();
+    DateTime MovieTimedates = format.parseDateTime(MovieTime);
 
-                day40 = MovieTimedates.plusDays(40);
-                String MovieTimeAft40days = format.print(day40);
+    day40 = MovieTimedates.plusDays(40);
+    String MovieTimeAft40days = format.print(day40);
 
+    dates.add(new FromEarliestToTheLatest(nameElement.getText(), MovieTimedates));
 
-                dates.add(new FromEarliestToTheLatest(nameElement.getText(), MovieTimedates));
+    Thread.sleep(15000);
+    //add 2nd page
+    driver.findElements(By.cssSelector(".pagebar a")).get(2).click();
 
-                //add 2nd page
-                driver.findElements(By.cssSelector(".pagebar a")).get(2).click();
+    WebElement nameElement12 = element.findElement(By.cssSelector("a"));
+    WebElement engNameElement12 = element.findElement(By.cssSelector("h3"));
+    WebElement timeNameElement12 = element.findElement(By.cssSelector("time"));
 
+    String MovieTime12 = timeNameElement12.getText();
+    DateTime MovieTimedates12 = format.parseDateTime(MovieTime12);
 
+    day40 = MovieTimedates12.plusDays(40);
+    String MovieTimeAft40days12 = format.print(day40);
 
+    dates.add(new FromEarliestToTheLatest(nameElement12.getText(), MovieTimedates12));
 
-                    System.out.print(nameElement.getText() + "\t");
-                    System.out.print(engNameElement.getText() + "\t");
-                    System.out.print(timeNameElement.getText() + "\t");
-                    System.out.print("Add 40 days :\t" + MovieTimeAft40days);
-                    printer.printRecord(nameElement.getText(),engNameElement.getText(),timeNameElement.getText(),MovieTimedates);
-                    //put into movie.csv
-                }
+    Thread.sleep(15000);
 
+    System.out.print(nameElement.getText() + "\t");
+    System.out.print(engNameElement.getText() + "\t");
+    System.out.print(timeNameElement.getText() + "\t");
+    System.out.print(nameElement12.getText()+"\t");
+    System.out.print(engNameElement12.getText()+"\t");
+    System.out.print(timeNameElement12.getText()+"\t");
 
-                System.out.println("\nFrom earliest to the latest :\n");
-                Collections.sort(dates, Comparator.comparing(FromEarliestToTheLatest::getDate));
-                for (FromEarliestToTheLatest date : dates) {
-                    System.out.println(date);
-                }
+    System.out.print("Add 40 days :\t" + MovieTimeAft40days);
+    System.out.print(MovieTimeAft40days12);
 
+    String P1TitlePlusP2Title=nameElement.getText()+nameElement12.getText();
+    String P1engPlusP2eng=engNameElement.getText()+engNameElement12.getText();
+    String P1TimePlusP2Time=timeNameElement.getText()+timeNameElement12.getText();
+    String P1Aft40DaysplusP2Aft40Days=MovieTimeAft40days+MovieTimeAft40days12;
+//
+//    printer.printRecord(nameElement.getText(),engNameElement.getText(),timeNameElement.getText(),MovieTimedates);
+    printer.printRecord(P1TitlePlusP2Title,P1engPlusP2eng,P1TimePlusP2Time,P1Aft40DaysplusP2Aft40Days);
 
-            }
+    //put into movie.csv
+        }
 
-   catch(NoSuchElementException e)
-            {
-                e.printStackTrace();
-            }
-   catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-   finally
-            {
-                driver.quit();
-            }
-            System.out.println("\n\n");
-            WebDriver driver2 = new ChromeDriver();
-            driver2.get("https://autos.yahoo.com.tw/new-cars/make/audi");
-
-            String title2 = driver2.getTitle();
-            System.out.println(title2);
-            for (int i = 0; i < 1500; i++) {
-                ((JavascriptExecutor) driver2).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-                //in order to automatically scroll down the page and load the data
-            }
-
-            try {
-
-                List<WebElement> elements2 = driver2.findElements(By.cssSelector(" .year-single"));
-
-                for (WebElement element : elements2) {
-
-                    WebElement nameElement = element.findElement(By.cssSelector("span.title"));
-                    WebElement priceElement = element.findElement(By.cssSelector("span.price"));
-
-
-                    System.out.print(nameElement.getText() + "\t");
-                    System.out.print("車價 :\t" + priceElement.getText() + "\t萬\n");
-                }
-
-
-            } catch (NoSuchElementException e) {
-                e.printStackTrace();
-            } finally {
-                driver2.quit();
-            }
+    System.out.println("\nFrom earliest to the latest :\n");
+    Collections.sort(dates, Comparator.comparing(FromEarliestToTheLatest::getDate));
+    for (FromEarliestToTheLatest date : dates) {
+        System.out.println(date);
+    }
 
 
     }
+
+catch(NoSuchElementException e)
+{
+    e.printStackTrace();
+}
+catch(IOException e)
+{
+    e.printStackTrace();
+}
+catch(InterruptedException e)
+{
+    e.printStackTrace();
+}
+   finally
+    {
+        driver.quit();
+    }
+    System.out.println("\n\n");
+    WebDriver driver2 = new ChromeDriver();
+    driver2.get("https://autos.yahoo.com.tw/new-cars/make/audi");
+
+    String title2 = driver2.getTitle();
+    System.out.println(title2);
+    for (int i = 0; i < 1500; i++) {
+        ((JavascriptExecutor) driver2).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        //in order to automatically scroll down the page and load the data
+    }
+
+    try {
+
+    List<WebElement> elements2 = driver2.findElements(By.cssSelector(" .year-single"));
+
+    for (WebElement element : elements2) {
+
+        WebElement nameElement = element.findElement(By.cssSelector("span.title"));
+        WebElement priceElement = element.findElement(By.cssSelector("span.price"));
+
+
+        System.out.print(nameElement.getText() + "\t");
+        System.out.print("車價 :\t" + priceElement.getText() + "\t萬\n");
+    }
+
+
+    } catch (NoSuchElementException e) {
+        e.printStackTrace();
+    } finally {
+        driver2.quit();
+    }
+
+
+}
 
 }
